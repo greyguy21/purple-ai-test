41f39a126e68c9a36ed764adfea096de0e6a4536
magdeline <85625305+MagdelineNg@users.noreply.github.com>
2023-07-11 15:57:58 +0800

    Update retrieveCatalog.js

--- retrieveCatalog.js
+++ retrieveCatalog.js
@@ -1,17 +1,17 @@
-const express = require("express");
+// const express = require("express");
 
-const app = express();
-// const port = process.env.SERVER_PORT || 8000;
+// const app = express();
+// // const port = process.env.SERVER_PORT || 8000;
 
-// Add Access Control Allow Origin headers
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+// // Add Access Control Allow Origin headers
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// });
 
 // app.listen(port, () => console.log(`Listening on port ${port}`));
 
