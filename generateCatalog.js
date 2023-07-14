0a86b3b32eea5af32f1096dd9a7c01b43bc2ce36
Georgetxm <xuanmingt@gmail.com>
2023-07-14 09:05:02 +0800

    Update: generateCatalog to fit into github actions pipeline and gitignore

--- generateCatalog.js
+++ generateCatalog.js
@@ -1,4 +1,7 @@
 const fs = require("fs");
+const { execSync } = require('child_process');
+
+const commitMessage = "Update catalog.json";
 
 const ruleIdsWithHtml = [
   "aria-hidden-focus",
@@ -14,36 +17,45 @@ const ruleIdsWithHtml = [
   "tabindex",
 ];
 
-const outputJson = {};
 
-// Look through each item in the array
-// open the respective file as per ruleId
-// read the file's contents
-// retrieve the keys of the object
-ruleIdsWithHtml.forEach((ruleId) => {
-  let file = null;
 
-  // Check if the file exists
-  if (fs.existsSync(`${__dirname}/results/${ruleId}.json`, "utf8")) {
-    file = fs.readFileSync(`${__dirname}/results/${ruleId}.json`, "utf8");
-  }
+const run = async () => {
+  const outputJson = {};
 
-  // If the file exists, parse the contents
-  if (file) {
-    const data = JSON.parse(file);
-    const keys = Object.keys(data);
-    keys.forEach((key) => {
-      if (!outputJson[ruleId]) {
-        outputJson[ruleId] = [];
-      } else {
-        outputJson[ruleId].push(key);
-      }
-    });
-  } else {
-    outputJson[ruleId] = [];
-  }
-});
+  // Look through each item in the array
+  // open the respective file as per ruleId
+  // read the file's contents
+  // retrieve the keys of the object
+  ruleIdsWithHtml.forEach((ruleId) => {
+    let file = null;
+  
+    // Check if the file exists
+    if (fs.existsSync(`${__dirname}/results/${ruleId}.json`, "utf8")) {
+      file = fs.readFileSync(`${__dirname}/results/${ruleId}.json`, "utf8");
+    }
+  
+    // If the file exists, parse the contents
+    if (file) {
+      const data = JSON.parse(file);
+      const keys = Object.keys(data);
+      keys.forEach((key) => {
+        if (!outputJson[ruleId]) {
+          outputJson[ruleId] = [];
+        } else {
+          outputJson[ruleId].push(key);
+        }
+      });
+    } else {
+      outputJson[ruleId] = [];
+    }
+  });
+  
+  outputJson["lastUpdated"] = new Date().toLocaleString();
+  
+  fs.writeFileSync("catalog.json", JSON.stringify(outputJson, null, 2));
+  
+  execSync(`git pull && git add catalog.json && git commit -m "${commitMessage}"`) 
+  execSync(`git push`)
+}
 
-outputJson["lastUpdated"] = new Date().toLocaleString();
-
-fs.writeFileSync("catalog.json", JSON.stringify(outputJson, null, 2));
+run();
